a0329ef9eb28c3991e1d8cf950eb38dc4a78b37a
Roman Lazarev <144658461+romalazar@users.noreply.github.com>
2024-03-13 21:43:47 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,29 @@
+
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+    const data = generateRandomData();
+function generateRandomData() {
+    }
+    data.forEach(item => {
+}
+function main() {
+
+    return data;
+    const data = [];
+}
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+main();public class RandomDataGenerator {
+
+}
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+        }
+        for (int item : data) {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+        }
+
+            System.out.println("Random Number: " + item);
